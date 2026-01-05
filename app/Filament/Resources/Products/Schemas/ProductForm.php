bd57323f28208b292b113f07a1af186cafd5722b
Lana_ushio noa <aldoklozen@gmail.com>
2026-01-05 18:45:29 +0700

    feat: initial commit - Laravel Filament E-Commerce

---
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Select; // ← tambahkan ini
+use Filament\Schemas\Schema;
+
+class ProductForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('$'),
+                Select::make('category_id') // ← diubah jadi Select
+                    ->relationship('category', 'name')
+                    ->required()
+                    ->searchable()
+                    ->preload(),
+            ]);
+    }
+}
